cfc285da71f0f80c847fac76092f04e1522fe905
ikerbotana02 <ikerbotana@usal.es>
2025-05-29 12:56:05 +0200

    dist añadido 2

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize("educa_quest", "root", "4236", {
+/*const sequelize = new Sequelize("educa_quest" , "root", "4236", {
     host: "localhost",
     dialect: "mysql"
+});*/
+const sequelize = new sequelize_1.Sequelize("railway", "root", "YMQKexHOTzYDAkmoUhVMuHjdQWOjVefn", {
+    host: "containers-us-west-xx.railway.app",
+    port: Number(3306),
+    dialect: 'mysql',
+    logging: false,
 });
 exports.default = sequelize;
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("educa_quest" , "root", "4236", {
+/*const sequelize = new Sequelize("educa_quest" , "root", "4236", {
     host: "localhost",
     dialect: "mysql"
-});
+});*/
+
+const sequelize = new Sequelize(
+  "railway",
+  "root",
+  "YMQKexHOTzYDAkmoUhVMuHjdQWOjVefn",
+  {
+    host: "containers-us-west-xx.railway.app",
+    port: Number(3306),
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 export default sequelize;
