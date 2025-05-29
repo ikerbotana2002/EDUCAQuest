98e6fe0df2275fe15374c468aec98a82c4b9a5e4
ikerbotana02 <ikerbotana@usal.es>
2025-05-29 15:33:53 +0200

    preparado demo

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -1,17 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-/*const sequelize = new Sequelize("educa_quest" , "root", "4236", {
+const sequelize = new sequelize_1.Sequelize("educa_quest", "root", "4236", {
     host: "localhost",
     dialect: "mysql"
-});*/
-const sequelize = new sequelize_1.Sequelize("railway", // DB_NAME
-"root", // DB_USER
-"YMQKexHOTzYDAkmoUhVMuHjdQWOjVefn", // DB_PASSWORD
-{
+});
+/*
+const sequelize = new Sequelize(
+  "railway", // DB_NAME
+  "root",    // DB_USER
+  "YMQKexHOTzYDAkmoUhVMuHjdQWOjVefn", // DB_PASSWORD
+  {
     host: "caboose.proxy.rlwy.net", // DB_HOST (público)
-    port: 21134, // puerto correcto
+    port: 21134,                    // puerto correcto
     dialect: "mysql",
     logging: false,
-});
+  }
+);*/
 exports.default = sequelize;
