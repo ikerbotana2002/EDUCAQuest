e808a7141117811cc6a6d9684c01326f466f5938
ikerbotana02 <ikerbotana@usal.es>
2025-07-13 11:30:36 +0200

    DEMO FINAL DEFENSA TFG

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,22 +1,22 @@
 import { Sequelize } from "sequelize";
 
-
+/*
 const sequelize = new Sequelize("educa_quest" , "root", "4236", {
     host: "localhost",
     dialect: "mysql"
 });
+*/
 
-/*
 const sequelize = new Sequelize(
   "railway", // nombre de la base de datos
   "root",    // usuario
-  "iluMDoDHqsjrlLSLiSSSAOXEvWIylkKD", // contraseña
+  "EJaKsYrOuvfIUuXIAFsdzzFAhukUozQm", // contraseña
   {
-    host: "metro.proxy.rlwy.net",
-    port: 15087,
+    host: "centerbeam.proxy.rlwy.net",
+    port: 19678,
     dialect: "mysql",
     logging: false
   }
-);*/
+);
 
 export default sequelize;
